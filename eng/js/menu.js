0d6b6c98c403e48f76e04e9b560267f02b9b1a7b
Seungyeol <marudev4905@gmail.com>
2022-06-01 16:46:10 +0900

    'eng_page_modify'

--- eng/js/menu.js
+++ eng/js/menu.js
@@ -3,17 +3,27 @@ $(document).ready(function(){
     var menuOpenBtn = $(".menu__open__btn");
     var menu = $(".menu");
     var menuCloseBtn = $(".close__btn");
+    var dim = $(".dim");
 
     menuOpenBtn.click(function(){
 
         menu.addClass("show");
 
+        dim.addClass("show");
     });
 
     menuCloseBtn.click(function(){
 
         menu.removeClass("show");
 
+        dim.removeClass('show');
+    });
+
+    dim.click(function(){
+
+        menu.removeClass("show");
+        
+        dim.removeClass("show");
     });
 
 });
--- eng/js/search.js
+++ eng/js/search.js
@@ -3,12 +3,23 @@ $(document).ready(function(){
     var searchOpenBtn = $(".search__open__btn");
     var search = $(".search");
     var searchCloseBtn = $(".search_close_btn");
+    var dim = $(".dim");
 
     searchOpenBtn.click(function(){
         search.addClass("show");
+        
+        dim.addClass("show");
     });
 
     searchCloseBtn.click(function(){
         search.removeClass("show");
+        
+        dim.removeClass("show");
+    });
+
+    dim.click(function(){
+        search.removeClass("show");
+
+        dim.removeClass("show");
     });
 });
